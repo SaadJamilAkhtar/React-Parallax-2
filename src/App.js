58bcc92a06216a1dd2107cbacb752623b05919bd
MuhammadSaadJamil <muhammadsaadjamil1@gmail.com>
2022-04-19 13:21:19 +0500

    added Style Tweaks

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import { useRef } from 'react';
+import {useRef} from 'react';
 import moon from './moon.png';
 import land from './land.png';
 import cat from './cat.gif';
 
-import { Parallax, ParallaxLayer } from '@react-spring/parallax';
+import {Parallax, ParallaxLayer} from '@react-spring/parallax';
 
 function App() {
-  const ref = useRef();
+    const ref = useRef();
 
-  return (
-      <div>
-        <Parallax pages={4} ref={ref}>
-          {/* <ParallaxLayer speed={1}>
+    return (
+        <div>
+            <Parallax pages={4} ref={ref}>
+                {/* <ParallaxLayer speed={1}>
             <h2>Welcome to my website</h2>
         </ParallaxLayer>
 
@@ -19,51 +19,52 @@ function App() {
             <h2>Web development is fun!</h2>
         </ParallaxLayer> */}
 
-          <ParallaxLayer
-              offset={0}
-              speed={1}
-              factor={2}
-              style={{
-                backgroundImage: `url(${moon})`,
-                backgroundSize: 'cover',
-              }}
-          />
+                <ParallaxLayer
+                    offset={0}
+                    speed={1}
+                    factor={2}
+                    style={{
+                        backgroundImage: `url(${moon})`,
+                        backgroundSize: 'cover',
+                        backgroundPosition: '-8rem 0rem'
+                    }}
+                />
 
-          <ParallaxLayer
-              offset={2}
-              speed={1}
-              factor={4}
-              style={{
-                backgroundImage: `url(${land})`,
-                backgroundSize: 'cover',
-              }}
-          ></ParallaxLayer>
+                <ParallaxLayer
+                    offset={2}
+                    speed={1}
+                    factor={4}
+                    style={{
+                        backgroundImage: `url(${land})`,
+                        backgroundSize: 'cover',
+                    }}
+                />
 
-          <ParallaxLayer
-              sticky={{ start: 0.9, end: 2.5 }}
-              style={{ textAlign: 'center' }}
-          >
-            <img src={cat} />
-          </ParallaxLayer>
+                <ParallaxLayer
+                    sticky={{start: 1, end: 2.95}}
+                    style={{textAlign: 'center'}}
+                >
+                    <img src={cat} style={{width: '25rem'}}/>
+                </ParallaxLayer>
 
-          <ParallaxLayer
-              offset={0.2}
-              speed={0.05}
-              onClick={() => ref.current.scrollTo(3)}
-          >
-            <h2>Welcome to my website</h2>
-          </ParallaxLayer>
+                <ParallaxLayer
+                    offset={0.2}
+                    speed={0.05}
+                    onClick={() => ref.current.scrollTo(3)}
+                >
+                    <h2>Welcome to my website</h2>
+                </ParallaxLayer>
 
-          <ParallaxLayer
-              offset={3}
-              speed={2}
-              onClick={() => ref.current.scrollTo(0)}
-          >
-            <h2>Hi Mom!</h2>
-          </ParallaxLayer>
-        </Parallax>
-      </div>
-  );
+                <ParallaxLayer
+                    offset={3.4}
+                    speed={2}
+                    onClick={() => ref.current.scrollTo(0)}
+                >
+                    <h2>The Parallax Effect</h2>
+                </ParallaxLayer>
+            </Parallax>
+        </div>
+    );
 }
 
 export default App;
